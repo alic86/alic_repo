352243435347ddb4b82870b8ce46a6905ca8552b
alic86 <alic86@sina.com>
2017-01-18 14:10:19 +0800

    change

--- third-part/src/main/java/com/alic/hadoop/MaxTemperatureMapper.java
+++ third-part/src/main/java/com/alic/hadoop/MaxTemperatureMapper.java
@@ -1,8 +1,7 @@
 package com.alic.hadoop;
 
 import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
 
 public class MaxTemperatureMapper extends MapReduceBase {
-
+	
 }
